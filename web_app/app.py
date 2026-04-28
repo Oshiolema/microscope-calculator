a7a436af62fe3f378bd6faa270e088e27d94c339
Oshiolema <morinsolaomoks@gmail.com>
2026-04-28 21:49:40 +0100

    Update app.py for Render deployment

--- web_app/app.py
+++ web_app/app.py
@@ -192,4 +192,4 @@ def remove_one_record(record_id):
 
 if __name__ == "__main__":
     initialise_database()
-    bioscope_app.run(debug=True)
+    bioscope_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
